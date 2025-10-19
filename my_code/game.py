557ef2594fe33e06bb70013c03b7760be48d6dd9
Carla Regina <carla.rlguimaraes@gmail.com>
2025-10-19 18:43:45 -0300

    SQLite3 implementation; score using desing; pattern

--- my_code/game.py
+++ my_code/game.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+import sys
 
 from my_code.menu import Menu, MENU_OPTION
 from my_code.const import WIN_HEIGHT, WIN_WIDTH
 from my_code.level import Level
+from my_code.score import Score
 
 
 class Game:
@@ -14,6 +16,7 @@ class Game:
 
     def run(self):   
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -24,15 +27,17 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
 
             elif menu_return == MENU_OPTION[3]:
-                pass 
+                score.show() 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit() # End pygame (fechar pygame)
             else:
                 pygame.quit() # Close window (fechar janela) 
-                exit() 
+                sys.exit() 
             
 
 
--- my_code/level.py
+++ my_code/level.py
@@ -87,6 +87,6 @@ class Level:
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name=F_NAME, size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color). convert_alpha()
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
